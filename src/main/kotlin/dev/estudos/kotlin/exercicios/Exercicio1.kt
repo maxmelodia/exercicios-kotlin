11f49f54e41e1de59c453cd8b500b45bf608604e
Maxwell Duarte <maxmelodia@gmail.com>
2021-01-30 10:08:53 -0300

    Exercicio 1 concluído

--- src/main/kotlin/dev/estudos/kotlin/exercicios/Exercicio1.kt
+++ src/main/kotlin/dev/estudos/kotlin/exercicios/Exercicio1.kt
@@ -8,39 +8,32 @@ import kotlin.math.sqrt
 //     fórmula que efetua tal cálculo é:
 //     d = RAIZ_QUADRADA( (x2 - x1)² + (y2 - y1)² )
 
-    fun main() {
+fun main() {
 
-        println("""
-            Programa para cálculo de distância entre dois pontos qualquer em um plano P(x1,y1) e P(x2,y2)
-            Informe os pontos x1,y1,x2,y2
-        """.trimIndent())
+    println("""
+        Programa para cálculo de distância entre dois pontos qualquer em um plano P(x1,y1) e P(x2,y2)
+        Informe os pontos x1,y1,x2,y2
+    """.trimIndent())
 
-        println("Digite o valor de x1: ")
-        val x1 = readLine()?.toInt()//? Ciente que pode dar nullpointer exception;
+    val x1 = readInt(msg="Digite o valor de x1: ")
+    val y1 = readInt(msg="Digite o valor de y1: ")
+    val x2 = readInt(msg="Digite o valor de x2: ")
+    val y2 = readInt(msg="Digite o valor de y2: ")
 
-        println("Digite o valor de y1: ")
-        val y1 = readLine()?.toInt()
+    var p1 = Ponto(x1!!, y1!!)//!!foça converão inteiro
+    var p2 = Ponto(x2!!, y2!!)//!!foça converão inteiro
 
-        println("Digite o valor de x2: ")
-        val x2 = readLine()?.toInt()
+    var resultado = calcularDistancia(p1, p2)
 
-        println("Digite o valor de y2: ")
-        val y2 = readLine()?.toInt()
+    println("Distancia Calculada: $resultado")
+    println("Fim!")
+}
 
-        var p1 = Ponto(x1!!, y1!!)//!!foça converão inteiro
-        var p2 = Ponto(x2!!, y2!!)//!!foça converão inteiro
+class Ponto(val x: Int, val y: Int)
 
-        var resultado = calcularDistancia(p1, p2)
-
-        println("Distancia Calculada: $resultado")
-        println("Fim!")
-    }
-
-    class Ponto(val x: Int, val y: Int)
-
-    fun calcularDistancia(p1: Ponto, p2: Ponto): Int {
-        val x2MenosX1 = (p2.x - p1.x).toDouble()
-        val y2MenosY1 = (p2.y - p1.y).toDouble()
-        val resultado = sqrt(x2MenosX1.pow(2) + y2MenosY1.pow(2))
-        return resultado.toInt()
-    }
+fun calcularDistancia(p1: Ponto, p2: Ponto): Int {
+    val x2MenosX1 = (p2.x - p1.x).toDouble()
+    val y2MenosY1 = (p2.y - p1.y).toDouble()
+    val resultado = sqrt(x2MenosX1.pow(2) + y2MenosY1.pow(2))
+    return resultado.toInt()
+}
---
+++ src/main/kotlin/dev/estudos/kotlin/exercicios/Utils.kt
@@ -0,0 +1,23 @@
+package dev.estudos.kotlin.exercicios
+
+import java.lang.NumberFormatException
+import kotlin.system.exitProcess
+
+fun readInt(msg:String): Int ?{
+    print(msg)
+
+    val valorDigitado = readLine()
+
+    if (valorDigitado.isNullOrBlank()) {
+        println("Nenhum valor informado. O programa será encerrado.")
+        exitProcess(0)
+    }
+
+    val valor = try {
+        valorDigitado?.toIntOrNull()
+    } catch (e: NumberFormatException){
+        println("Informe um valor correto!")
+        return readInt(msg)
+    }
+    return valor
+}
